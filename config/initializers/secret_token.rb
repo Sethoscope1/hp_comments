925d5cefe7d052f6d592778baa07192b7a337b9d
Seth Helfgott <sethleland@gmail.com>
2014-03-02 22:49:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HpComments::Application.config.secret_token = 'c56af685b89ef5fc6d3a240da13b2a60c0dd6da0f0fb0154bb620bdb7f6f00f8ba11c11b12ae67cbc6e9f0c989652d60e649a37f166f82c2c284de86f5a7cef2'
